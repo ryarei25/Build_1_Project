92e9780e3175e6e30f22ebafe845161a82a5aaf0
ryarei25 <aryamreiland@gmail.com>
2025-09-17 14:08:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,23 +126,11 @@ header {
     flex-direction: column;
 }
 
-/* --- Inputs & selectbox --- */
-.stTextInput>div>div>input,
-.stTextArea>div>div>textarea,
-.stSelectbox>div>div>div[role="listbox"],
-select, option {
-    background-color: #D9FEC9 !important;  /* light green vibe dropdown */
-    border: 2px solid #D8C5DD;
-    border-radius: 6px;
-    color: #333;
-    font-family: 'Comfortaa', cursive;
-    padding: 6px;
-}
 
 
 /* --- Selectboxes & dropdowns --- */
 .stSelectbox>label {
-    color: #333 !important;  /* make "Choose a model" label black */
+    color:  #000 !important;  /* make "Choose a model" label black */
 }
 
 .stSelectbox>div>div>div[role="selectbox"],
@@ -150,7 +138,7 @@ select, option {
     background-color: #D9FEC9 !important;  /* light green vibe dropdown */
     border: 2px solid #D8C5DD;
     border-radius: 6px;
-    color: #333;
+    color: #D9FEC9;
     font-family: 'Comfortaa', cursive;
     padding: 6px;
 }
